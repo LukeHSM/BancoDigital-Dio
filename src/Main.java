24d7b6152a0ec03a9bb7817556888862060a7be6
Lucas Henrique Silva Martins <lucashsmartins94@gmail.com>
2025-05-15 14:41:09 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// interface Conta: sacar(), depositar(), extrato() e transferir();
+// ContaBancaria implementa a interface
+// ContaCorrente e (ContaPoupança???) extendem de ContaBancaria
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
